7b81566ee2b0e780777875002cf677d1b1a3620c
Hub Desenvolvedores <hubdesenv@aluno.ms.senac.br>
2024-08-08 21:34:45 -0400

    corrigindo construtores init

--- classes.py
+++ classes.py
@@ -4,9 +4,9 @@ class Itens:
     valor_produto = float
 
     def __init__(self,nome_produto,quantidade_produto,valor_produto):
-        nome_produto = nome_produto   
-        quantidade_produto = quantidade_produto 
-        valor_produto = valor_produto
+        self.nome_produto = nome_produto   
+        self.quantidade_produto = quantidade_produto 
+        self.valor_produto = valor_produto
 
 class Produtos:
     nome_produto = ''
@@ -14,21 +14,21 @@ class Produtos:
     quantidade_estoque = int
 
     def __init__(self,nome_produto,preco,quantidade_estoque):
-        nome_produto = nome_produto
-        preco = preco
-        quantidade_estoque = quantidade_estoque
+        self.nome_produto = nome_produto
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
 
 class Cliente:
     nome = ''
 
     def __init__(self,nome):
-        nome = nome
+        self.nome = nome
 
 class Admin:
     nome = ''
     codigo_adm = int
 
     def __init__(self,nome,codigo_adm):
-        nome = nome
-        codigo_adm = codigo_adm
+        self.nome = nome
+        self.codigo_adm = codigo_adm
 
