e245c365f80de2eb4bd90bce7707a92e4c6b2eb1
Hub Desenvolvedores <hubdesenv@aluno.ms.senac.br>
2024-08-08 21:18:48 -0400

    adicionando construtor na classe itens

--- classes.py
+++ classes.py
@@ -2,12 +2,22 @@ class Itens:
     nome_produto = ''
     quantidade_produto = ''
     valor_produto = float
-    valor_total = float
+
+    def __init__(self,nome_produto,quantidade_produto,valor_produto):
+        nome_produto = nome_produto   
+        quantidade_produto = quantidade_produto 
+        valor_produto = valor_produto
+
+    def adicionar_produto():
+        pass
 
 class Produtos:
     nome_produto = ''
     preco = float
     quantidade_estoque = int
+
+    def __init__():
+        
 
 class Usuario:
     nome = ''
