31abf4f867a82c6d7ddf2151b712787beb0d916c
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2024-08-08 21:26:32 -0400

    Primeira versão Carrinho

--- compras.py
+++ compras.py
@@ -1,20 +1,33 @@
-from menu import *
+from menus import *
 from classes import *
 from time import sleep
+import os
 carrinho = []
 op = 's'
 while op.upper() !='N':
+    os.system("cls")
     lista_produtos()
     print('Escolha seu produto:')
     cod_pro = int(input('Código do produto: '))
     qtd = int(input('Quantidade: '))
-    item = Item(qtd, prod, valor_unit, valor_total)
+    while qtd > produtos[cod_pro-1].quantidade_estoque:
+        print('Quantidade solicitada não está disponível em estoque!!')
+        qtd = int(input('Por favor, redefina a quantidade: '))
+    prod = produtos[cod_pro-1].nome_produto
+    valor_unit = produtos[cod_pro-1].preco
+    #Definir campos da classe produto para percorrer a lista: Nome, preço e qtd_estoque
+    item = Itens(qtd, prod, valor_unit) #Adicionar objeto itens a uma lista que será o carrinho
     carrinho.append(item)
+    #Baixar a quantidade em estoque
     op = input('Deseja continuar (s/n): ')
+os.system("cls")
 print(50*'-')
 print('         Carrinho de compras:')
 print(50*'-')
+print('Qtd. Produto                 Valor unit.      Valor Total')
+total_carrinho = float(0)
 for item in carrinho:
-    print(f'{item.quantidade} - {item.descricao}')
-
-
+    print(f'{item.quantidade} - {item.nome} - R$ {item.preco}  R$ {item.preco*item.quantidade}')
+    total_carrinho += item.preco*item.quantidade
+print(50*'-')
+print(f'Valor total........................... R$ {total_carrinho:.2f}')
