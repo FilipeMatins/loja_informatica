2fb1154be492d3f744b049fcc1240d005669ea88
Adercio Barbuio Junior <barbuiojr@hotmail.com>
2024-08-09 14:59:37 -0400

    Alterações em métodos e compras

--- compras.py
+++ compras.py
@@ -2,32 +2,35 @@ from menu import *
 from classes import *
 from time import sleep
 import os
-def iniciar_compra():
+def iniciar_compra(produtos):
     carrinho = []
     op = 's'
     while op.upper() !='N':
         os.system("cls")
-        lista_produtos()
+        Produtos.lista_produtos(produtos)
         print('Escolha seu produto na lista acima')
         cod_pro = int(input('Código do produto: '))
         qtd = int(input('Quantidade: '))
-        while qtd > produtos[cod_pro-1].quantidade_estoque:
+        while qtd > int(produtos[cod_pro-1].quantidade_estoque):
             print('Quantidade solicitada não está disponível em estoque!!')
             qtd = int(input('Por favor, redefina a quantidade: '))
         prod = produtos[cod_pro-1].nome_produto
         valor_unit = produtos[cod_pro-1].preco
         item = Itens(prod, qtd, valor_unit)
         carrinho.append(item)
-        produtos[cod_pro-1].quantidade_estoque = produtos[cod_pro-1].quantidade_estoque - qtd #Baixar a quantidade em estoque
+        q_est = int(produtos[cod_pro-1].quantidade_estoque)
+        q_est -= qtd #Baixar a quantidade em estoque
+        produtos[cod_pro-1].quantidade_estoque = q_est
         op = input('Deseja continuar (s/n): ')
     os.system("cls")
     print(50*'-')
-    print('         Carrinho de compras:')
+    print('               Carrinho de Compras:')
     print(50*'-')
-    print('Qtd. Produto                 Valor unit.      Valor Total')
+    print('Qtd. Produto                 Valor unit.  Valor Total')
     total_carrinho = float(0)
     for item in carrinho:
-        print(f'{item.quantidade} - {item.nome} - R$ {item.preco}  R$ {item.preco*item.quantidade}')
-        total_carrinho += item.preco*item.quantidade
+        total_item = float(item.valor_produto)*float(item.quantidade_produto)
+        print(f'{item.quantidade_produto:<5}{item.nome_produto:<24} R$ {float(item.valor_produto):.2f}  R$ {total_item:.2f}')
+        total_carrinho += total_item
     print(50*'-')
-    print(f'Valor total........................... R$ {total_carrinho:.2f}')
+    print(f'Valor total................................ R$ {total_carrinho:.2f}')
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 
 #import *
 from menu import *
+from compras import *
+from classes import *
 
+#Busca de produtos no arquivo de texto
+produtos = []
+with open('lista_produtos.txt', 'r', encoding='utf-8') as list_prod:
+    dados = list_prod.readlines()
+    for i in range(0, len(dados), 3):
+        n = dados[i].rstrip('\n')
+        p = dados[i+1].rstrip('\n')
+        q = dados[i+2].rstrip('\n')
+        prod_atual = Produtos(n,p,q)
+        produtos.append(prod_atual)
 # fluxo_principal()
 while True:
     login()
@@ -12,10 +24,11 @@ while True:
             menu_loja()
             op_comprador = int(input('Escolha uma opção: '))
             if op_comprador == 1:
-                #função
-                pass
+                os.system("cls")
+                Produtos.lista_produtos(produtos)
+                
             elif op_comprador == 2:
-                #função
+                iniciar_compra(produtos)
                 pass
             elif op_comprador == 99:
                 break
